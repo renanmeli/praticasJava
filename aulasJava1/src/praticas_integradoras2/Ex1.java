fc1c441b4015b2907e44c1f10d559540f45b2201
renanmeli <renan.rsantana@mercadolivre.com>
2021-12-18 18:12:43 -0300

    Pratica integradora 2

---
+++ aulasJava1/src/praticas_integradoras2/Ex1.java
@@ -0,0 +1,18 @@
+package praticas_integradoras2;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Ex1 {
+    public static void main(String[] args) {
+
+        Integer[] array ={52,10,85,1324,01,13,62,30,12,127};
+        System.out.println("Original: "+ Arrays.toString(array));
+
+        Arrays.sort(array);
+        System.out.println("Crescente: " + Arrays.toString(array));
+
+        Arrays.sort(array, Collections.reverseOrder());
+        System.out.println("Decrescente: " + Arrays.toString(array));
+    }
+}
